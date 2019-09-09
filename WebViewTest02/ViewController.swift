08e5673d03120b6481259f96c802169d854e0af2
Jonghyun Kim <jhkim@dit.ac.kr>
2019-09-09 14:18:28 +0900

    Initial Commit

---
+++ WebViewTest02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebViewTest02
+//
+//  Created by 김종현 on 09/09/2019.
+//  Copyright © 2019 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
